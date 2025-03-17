c5f5ccc42de7954f84061098c2c281a1c9116439
Leo-Dev <leonardocaimmi1@gmail.com>
2025-03-17 00:24:19 -0300

    JWT impl

---
+++ jwt/src/main/java/com/security/jwt/config/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package com.security.jwt.config.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+public class RestAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException)
+            throws IOException {
+
+        String message = authException.getMessage();
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write(
+                "{\"error\": \"No autorizado\", \"mensaje\": \"" + message + "\", \"timestamp\": \"" + LocalDateTime.now() + "\"}"
+        );
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        setRealmName("JWT");
+        super.afterPropertiesSet();
+    }
+}
